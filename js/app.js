bcf92a250c2d1b2a4f33d78159c98b9777304427
daveharmswebdev <wdave.harms@gmail.com>
2016-01-18 22:34:20 -0600

    a lot of function a lot of bugs

--- js/app.js
+++ js/app.js
@@ -20,29 +20,32 @@ $( document ).ready(function() {
 
   function addItem() {
     var gitem = $('input').val();
-    console.log(gitem);
-    $('#'+selectStore).append("<li><div class=\"item\"><p>"+gitem+"</p><i class=\"fa fa-minus-square\"></i></div></li>");
+    // console.log(gitem);
+    $('#'+selectStore).append("<li><div class=\"item uncheck\"><i class=\"fa fa-check-square\"></i><p>"+gitem+"</p><i class=\"fa fa-minus-square\"></i></div></li>");
     $('input').val('');
   }
 
   function addStore(store) {
     var idStore = store.replace(/\s+/g, '');
-    $('.stores').append('<h3 class=\'notselected\'>'+store+'</h3>').append('<ul id=\''+idStore+'\'></ul>');
+    $('.stores').append('<h3 class=\'notselected\'>'+store+'</h3>').append('<ul id=\''+idStore+'\' class=\'connectedSortable\'></ul>');
     $('ul').sortable({containment: 'parent',
-      tolerance: 'pointer'
+      tolerance: 'pointer', connectWith: '.connectedSortable'
     });
-    $('ul').on('click', 'i', function() {
+    $('ul').on('click', '.fa-minus-square', function() {
         $(this).parents('.item').remove();
+    });
+    $('ul').on('click', '.fa-check-square',function() {
+      $(this).closest('div').toggleClass('check uncheck');
     });
     $('h3').click(function() {
       selectStore = this.innerHTML;
       selectStore = selectStore.replace(/\s+/g, '');
       var hstore = $(this);
-      console.log($(this));
-      console.log(hstore);
+      // console.log($(this));
+      // console.log(hstore);
       if (hstore.is('.notselected')) {
         hstore.removeClass('notselected').addClass('selected');
-        hstore.siblings().removeClass('selected').addClass('notselected');
+        hstore.next().removeClass('selected').addClass('notselected');
       }
     });
   }
