6742600052736721f38727e64aa35f7da7a7d68a
daveharmswebdev <wdave.harms@gmail.com>
2016-01-18 20:53:12 -0600

    selected individual stores

--- js/app.js
+++ js/app.js
@@ -2,11 +2,6 @@ $( document ).ready(function() {
 
   var selectStore;
 
-  // add item click button and val of input becomes next item
-  // $('.addItem').click(function() {
-  //   addItem();
-  // });
-
   $('.addItem').on('click', function() {
     addItem();
   });
@@ -20,30 +15,35 @@ $( document ).ready(function() {
   $('.addStore').click(function() {
     var gstore = $('input').val();
     addStore(gstore);
+    $('input').val('');
   });
 
   function addItem() {
     var gitem = $('input').val();
     console.log(gitem);
-    $('ul').append("<li><div class=\"item\"><p>"+gitem+"</p><i class=\"fa fa-minus-square\"></i></div></li>");
+    $('#'+selectStore).append("<li><div class=\"item\"><p>"+gitem+"</p><i class=\"fa fa-minus-square\"></i></div></li>");
     $('input').val('');
   }
 
   function addStore(store) {
     var idStore = store.replace(/\s+/g, '');
-    $('.stores').append('<h3>'+store+'</h3>')
-      .append('<ul id=\''+idStore+'\'></ul>');
+    $('.stores').append('<h3 class=\'notselected\'>'+store+'</h3>').append('<ul id=\''+idStore+'\'></ul>');
     $('ul').sortable({containment: 'parent',
       tolerance: 'pointer'
     });
     $('ul').on('click', 'i', function() {
-  //    $(this).parents('.item').css('background-color', 'yellow');
         $(this).parents('.item').remove();
     });
     $('h3').click(function() {
       selectStore = this.innerHTML;
       selectStore = selectStore.replace(/\s+/g, '');
-      console.log(selectStore);
+      var hstore = $(this);
+      console.log($(this));
+      console.log(hstore);
+      if (hstore.is('.notselected')) {
+        hstore.removeClass('notselected').addClass('selected');
+        hstore.siblings().removeClass('selected').addClass('notselected');
+      }
     });
   }
 
